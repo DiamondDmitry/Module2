0673a4f7a160e4c0cb6b4b6608f9f764908d6ed7
Dmitry Miroshnichenko <diamoney@outlook.com>
2024-03-06 13:44:10 -0600

    Exception and extension home tasks

---
+++ Module2/Extensions/ListRemoveDuplicatesExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2.Extensions
+{
+    public static class ListRemoveDuplicatesExtension
+    {
+        public static List<string> RemoveDuplicates(this List<string> fruitsList)
+        {
+            List<string> clearList = new List<string>();
+            foreach (string fruit in fruitsList)
+            {
+                if (!clearList.Contains(fruit))
+                {
+                    clearList.Add(fruit);
+                }
+            }
+            return clearList;
+        }
+    }
+}
--- Module2/Program.cs
+++ Module2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using Module2.Extensions;
+using System.Security.Cryptography;
 
 namespace Module2
 {
@@ -10,8 +11,7 @@ namespace Module2
             // Задача 1: Деление на ноль
             Console.WriteLine("Введите первое целое число:");
             int.TryParse(Console.ReadLine(), out int number1);
-            bool loop = false;
-
+            bool loop = true;
             do
             {
                 Console.WriteLine();
@@ -20,14 +20,13 @@ namespace Module2
 
                 try
                 {
-                    double result = (double)number1 / number2;
+                    int result = number1 / number2;
                     Console.WriteLine("Результат деления:" + result);
                     loop = false;
                 }
                 catch (DivideByZeroException ex)
                 {
                     Console.WriteLine("Деление на ноль невозможно!");
-                    loop = true;
                 }
             }
             while (loop);
@@ -35,28 +34,55 @@ namespace Module2
             // Задача 2: Обработка исключения
             try
             {
-            File.Open("test.txt", FileMode.Open);
+                File.Open("test.txt", FileMode.Open);
             }
             catch (FileNotFoundException ex)
-            { 
-                Console.WriteLine("Файл по указанному пути не найден!");
+            {
+                Console.WriteLine("Файл не найден!");
             }
 
             // Задача 3: Обработка ошибок при парсинге чисел
             try
             {
+                Console.WriteLine();
                 Console.WriteLine("Введите число:");
                 int number = Int32.Parse(Console.ReadLine());
-            } 
+            }
             catch (FormatException ex)
-            { 
+            {
                 Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
             }
 
             // Расширение типов.
             // Задача 1: Расширение для строк
+            Console.WriteLine();
+            Console.WriteLine("Введите предложение из нескольких слов:");
+            string sentence = Console.ReadLine();
+            Console.WriteLine($"В вашем предложении {sentence.CountWordsInString()} слов");
+
             // Задача 2: Расширение для чисел
+            Console.WriteLine("Введите целое число:");
+            int numberEvenCheck = Int32.Parse(Console.ReadLine());
+            if (numberEvenCheck.IsNumberEven())
+            {
+                Console.WriteLine($"Число \"{numberEvenCheck}\" четное");
+            }
+            else
+            {
+                Console.WriteLine($"Число \"{numberEvenCheck}\" не четное");
+            }
+
             // Задача 3: Расширение для списков
+            string[] fruitsArray = {"apple", "banana", "kiwi", "orange", "banana", "grape",
+                                    "red dragon", "orange", "banana","kiwi", "tangerin", "banana"};
+            List<string> fruits = new List<string>(fruitsArray);
+
+            Console.WriteLine();
+            List<string> clearFruitsList = fruits.RemoveDuplicates();
+            foreach (string fruit in clearFruitsList)
+            {
+                Console.WriteLine(fruit);
+            }
         }
     }
 }
